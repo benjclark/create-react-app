d9e8aa69741a70a3b31c39f1880b55a177267000
Ben Clark <hello@clarkben.com>
2020-02-09 14:55:00 +0000

    Begins to add fuzzy matching react component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import FuzzyMatchInput from "./FuzzyMatchInput";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="fuzzy-match-container">
+        <FuzzyMatchInput answer="grape"/>
+      </div>
     </div>
   );
 }
---
+++ src/FuzzyMatchInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function FuzzyMatchInput() {
+  return (
+    <input type="text" value="type your guess" />
+  );
+}
+
+export default FuzzyMatchInput;
